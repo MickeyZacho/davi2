1876f6a8f3b477284681e5b44243b94d2b33b510
unknown <au587515@uni.au.dk>
2021-11-30 20:20:12 +0100

    Parameters

--- src/algorithms/closestCity.js
+++ src/algorithms/closestCity.js
@@ -1,4 +1,5 @@
 import { kdTree } from "kd-tree-javascript"
+import { Button } from "@mui/material";
 import { FormControl, FormControlLabel, FormLabel, Checkbox, Slider, FormGroup } from '@mui/material';
 import * as React from 'react';
 
@@ -32,14 +33,42 @@ export class ClosestCity{
         return outData
     }
 
-    static getParameters() {
+    static getParameters(setAlgo) {
+        const [val, setVal] = React.useState({});
+
+        function handleChange1(e){
+            setVal((s) => ({
+                ...s,
+                check1: e.target.checked
+            }));
+        }
+        function handleChange2(e){
+            setVal((s) => ({
+                ...s,
+                check2: e.target.checked
+            }));
+        }
+
+        function handleClick(e){
+            e.preventDefault();
+            console.log("handleClick", document.getElementById('check1').checked, document.getElementById('check2').checked)
+            const c1 =document.getElementById('check1').checked;
+            const c2 =document.getElementById('check2').checked
+            setAlgo(
+                e,
+                {check1: c1,
+                check2: c2}
+            )
+        }
+
         return (
             <div style={{width: 500, justifyContent:"center", alignItems:"center"}}>
                 <span style={{ fontSize: 'medium',}}>Closest City Parameters</span>
                 <FormGroup>
-                    <FormControlLabel control={<Checkbox defaultChecked sx={{ '& .MuiSvgIcon-root': { fontSize: 20 } }} />} label={<span style={{ fontSize: 'small' }}>{"Label"}</span>} />
-                    <FormControlLabel disabled control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 20 } }} />} label={<span style={{ fontSize: 'small' }}>{"Disabled"}</span>} />
+                    <FormControlLabel control={<Checkbox defaultChecked id="check1" onChange={handleChange1} sx={{ '& .MuiSvgIcon-root': { fontSize: 20 } }} />} label={<span style={{ fontSize: 'small' }}>{"Label"}</span>} />
+                    <FormControlLabel disabled control={<Checkbox id="check2" onChange={handleChange2} sx={{ '& .MuiSvgIcon-root': { fontSize: 20 } }} />} label={<span style={{ fontSize: 'small' }}>{"Disabled"}</span>} />
                 </FormGroup>
+                <Button variant="outlined" onClick={handleClick}>Reload</Button>
             </div>
         )
     }
