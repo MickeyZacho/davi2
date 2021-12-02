24750cdb884afb7b62361c427028f01553fae65e
Jacob Andersen <201708233@post.au.dk>
2021-12-02 18:19:29 +0100

    fuck folders

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOXTOKEN;
 const DATA_URL = "./worldcities3.csv";
 const HOTEL_URL = "./hotelsout/";
 const DATA_PATH = "./CountryPop/";
+
 const COLOR_HOTEL = [166,206,227];
 const COLOR_CITY = [178,223,138];
 const COLOR_FIRST_ALGORITHM = [27,158,119]
@@ -218,26 +219,14 @@ export default () => {
         "Laos",
       ];
       let result = [];
-      var counter = 0;
       for (var i = 0; i < countries.length; i++) {
         await d3.csv(DATA_PATH + countries[i] + ".csv").then((data) => {
           for (var j = 0; j < data.length; j++) {
             if (data[j].population < 10000) continue;
             result.push(data[j]);
-            counter++;
           }
         });
       }
-      //console.log("counter: " + counter);
-      /*var citybox = {
-        Topleftlat: 59.6260769571419,
-        Topleftlong: -13.21956641460763,
-        Bottomrightlat: 55.10115713809608,
-        Bottomrightlong:12.96270706010456
-      }
-
-      const result = await apiBase.get(`API/V1/Cities?topLeft.Lat=${citybox.Topleftlat}&topLeft.Lon=${citybox.Topleftlong}&bottomRight.Lat=${citybox.Bottomrightlat}&bottomRight.Lon=${citybox.Bottomrightlong}`)
-      */
       const points = result.map(function (d) {
         //console.log(d);
         //console.log(d.city)
@@ -281,196 +270,13 @@ export default () => {
       setHotelData(points);
       setPosToCountry(new CountryFinder(points));
     };
-    /*const fetchData3 = async () => {
-      const countries = ["Denmark", "Sweden", "Norway", "Hong Kong", "Singapore", "Liechtenstein", "Luxembourg", "Iceland", "Turkey", "Poland", "Finland", "Netherlands", "Greece", "Germany", "United States", "United Kingdom", "Ireland", "France", "Spain", "Portugal", "Korea, South", "China", "Indonesia", "Belgium", "Italy", "Austria", "Slovakia", "Hungary", "Romania", "Moldova", "Serbia", "Bosnia And Herzegovina", "Slovenia", "Czechia", "Switzerland", "Macedonia", "Albania","Bulgaria", "Kosovo", "Croatia", "Ukraine", "Belarus", "Lithuania", "Latvia", "Estonia", "Georgia", "Japan", "Thailand", "Taiwan", "Vietnam", "Philippines", "Romania", "Malaysia", "India", "Canada", "Cambodia", "Laos"]
-      let result = []
-      var counter = 0;
-      for (var i = 0; i < countries.length; i++){
-        await d3.csv(DATA_PATH + countries[i] + ".csv").then((data) => {
-          for (var j = 0; j < data.length; j++) {
-            if(data[j].population < 10000) continue;
-            result.push(data[j])
-            counter++;
-          }
-        });
-      };
-      console.log("counter: " +  counter)
-      const points = result.map(function (d) {
-        return {
-          CityName: d.city,
-          position: [+(+d.lng - 1), +(+d.lat - 1) ],
-          population: d.population,
-          country: d.country
-        };
-      });
-      const finalpoints = points.filter((point) => point.population !== "");
-      console.log("GOT HERE");
-      setSecondData(finalpoints);
-    };
-    fetchData3()*/
+    
     fetchData();
     fetchData2();
   }, []);
 
   useEffect(() => {
     console.log("Calculating")
-    
-    /*function calculateVor(data) {
-      //const point = data.map(d => d.position);
-      const delau = Delaunay.from(
-        data,
-        (d) => d.position[0],
-        (d) => d.position[1]
-      );
-
-      const vor = delau.voronoi([-179, -89, 179, 89]);
-
-      const it = vor.cellPolygons();
-      let res = it.next();
-
-      const kdt = new kdTree(
-        [],
-        (a, b) =>
-          Math.sqrt(Math.pow(a.lat - b.lat, 2) + Math.pow(a.lng - b.lng, 2)),
-        ["lat", "lng"]
-      );
-      //console.log(kdt)
-      data.forEach((e) => {
-        let nPos = e.position;
-        let entry = {
-          lng: nPos[0],
-          lat: nPos[1],
-          cityName: e.CityName,
-          country: e.country,
-          cityPos: e.cityPos
-        };
-        kdt.insert(entry);
-      });
-
-      const lineMap = new Map();
-      let cityLines = new Map();
-
-      while (!res.done) {
-        //For every cell in the voronoi, calculate the corresponding hotel, by looking in the kd-tree of hotels
-        let averageX =
-          res.value.reduce((acc, c) => acc + c[0], 0) / res.value.length;
-        let averageY =
-          res.value.reduce((acc, c) => acc + c[1], 0) / res.value.length;
-
-        let near = kdt.nearest({ lng: averageX, lat: averageY }, 1)[0][0]; //returns a hotel data point {lng, lat, cityName, country}
-
-        for (let i = 0; i < res.value.length; i++) {
-          //Calculate all lines drawn, for a cell, including the last element to the first element
-          let posA = res.value[i];
-          let posB = res.value[(i + 1) % res.value.length];
-          //Create a path object, with the corresponding city and country of the hotel
-          let path = {
-            a: posA,
-            b: posB,
-            cityName: near.cityName,
-            country: near.country,
-            cityPos: near.cityPos
-          };
-          //Check for wierd case, where start and end position is the same
-          if (posA[0] === posB[0] && posA[1] === posB[1]) continue;
-          //Check if path is on the border
-          if (
-            (posA[0] === 179 && posB[0] === 179) ||
-            (posA[0] === -179 && posB[0] === -179) ||
-            (posA[1] === 89 && posB[1] === 89) ||
-            (posA[1] === -89 && posB[1] === -89)
-          ) {
-            if (cityLines.get(near.cityName) === undefined)
-              cityLines.set(near.cityName, []);
-            cityLines
-              .get(near.cityName)
-              .push({ a: posA, b: posB, sameCountry: true , cityPos: near.cityPos});
-            continue;
-          }
-          //Identify a path by the sum of the lat and long values, hopefully being unique, for faster search, and to equal a path from a to b and from b to a
-          let a = posA[0] + posA[1];
-          let b = posB[0] + posB[1];
-          let id = "" + posA[0] + posA[1] + posB[0] + posB[1]
-          if (a < b) id = "" + posB[0] + posB[1] + posA[0] + posA[1]
-          let nPath = lineMap.get(id)
-          if (nPath === undefined)
-            lineMap.set(id, path);
-          //When inserting, we return null if there wasn't an already existing element, otherwise we return the already existing, meaning we found a match
-          //Check if the city of the found path is different from the current cell, otherwise we don't want to draw the path
-
-          if (nPath === undefined || nPath.cityName === near.cityName) continue;
-          //If city path array not initialised, initialize them
-          if (cityLines.get(near.cityName) === undefined)
-            cityLines.set(near.cityName, []);
-          if (cityLines.get(nPath.cityName) === undefined)
-            cityLines.set(nPath.cityName, []);
-          //Add the path to the cityLines, and check if the line splits two countries
-          cityLines
-            .get(near.cityName)
-            .push({
-              a: posA,
-              b: posB,
-              sameCountry: near.country === nPath.country,
-              cityPos: near.cityPos 
-            });
-          cityLines
-            .get(nPath.cityName)
-            .push({
-              a: nPath.a,
-              b: nPath.b,
-              sameCountry: near.country == nPath.country,
-              cityPos: nPath.cityPos
-            });
-        }
-        res = it.next();
-      }
-      return cityLines;
-    }
-    function calculatePolygons(data) {
-      //let polygonMap = new Map();
-      let polys = []
-      data.forEach((value, key) => {
-        //console.log(key);
-        //if(key === "No City") return []
-        let entry = value[0];
-        value.splice(value.indexOf(entry), 1);
-        let polyCount = 0;
-        let currentPos = entry.b;
-        let path = [];
-        path.push([entry.a, entry.b]);
-        do {
-          let next = value.find((e) => {
-            return e.a[0] === currentPos[0] && e.a[1] === currentPos[1];
-          });
-
-          if (next !== undefined) {
-            value.splice(value.indexOf(next), 1);
-            path[polyCount].push(next.b);
-            currentPos = next.b;
-          } else {
-            polys.push({CityName: key, polygon: path[polyCount], cityPos: entry.cityPos})
-            
-            //console.log(value);
-            //path[polyCount].push(entry.a)
-            entry = value[0];
-            value.splice(value.indexOf(entry), 1);
-            currentPos = entry.b;
-            path.push([entry.a, entry.b]);
-            polyCount += 1;
-          }
-          polys.push({CityName: key, polygon: path[polyCount], cityPos: entry.cityPos})
-        } while (value.length > 0);
-
-        //console.log(path);
-       // polygonMap.set(key, path);
-        //polygonMap.set(key, path);
-      });
-      //testing code
-      //let polys = []
-      //polygonMap.forEach(value => value.forEach(v => polys.push(v)))
-      return polys;
-      //return polygonMap;
-    }*/
 
     let procData = Algorithms.algorithmStateSwitch(firstAlgorithmValue.value, countryCityData, countryHotelData, firstAlgorithmValue.parameters)
     let procData2 = Algorithms.algorithmStateSwitch(secondAlgorithmValue.value, countryCityData, countryHotelData, secondAlgorithmValue.parameters)
